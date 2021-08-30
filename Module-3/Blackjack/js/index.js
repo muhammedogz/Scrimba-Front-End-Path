75f6250b79bf9691af8a74d90b950b363b17af02
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-08-31 02:38:08 +0300

    Add functions

---
+++ Module-3/Blackjack/js/index.js
@@ -0,0 +1,51 @@
+let num1 = generateRandom();
+let num2 = generateRandom();
+let cards = [num1, num2];
+
+let messageEl = document.getElementById("message-el");
+let cardsEl = document.getElementById("cards-el");
+let sumEl = document.getElementById("sum-el");
+
+function startGame () {
+    renderGame();
+}
+
+function renderGame() {
+    let sum = 0;
+
+    let newCardsContent = "Cards: ";
+    for(let i = 0; i < cards.length; i++)
+    {
+        newCardsContent += cards[i] + " ";
+        sum += cards[i];
+    }
+    cardsEl.textContent = newCardsContent;
+    sumEl.textContent = "Sum: " + sum;
+
+    if (sum < 21) 
+    {
+        messageEl.textContent = "Do you want to take another card?";
+    }
+    else if (sum == 21) 
+    {
+        messageEl.textContent = "It's Blackjack!";
+    }
+    else
+    {
+        messageEl.textContent = "You lose.";
+    }
+    
+}
+
+function newCard() {
+    cards.push(generateRandom());
+    renderGame();
+}
+
+
+
+
+
+function generateRandom() {
+    return 5;
+}
