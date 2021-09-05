a2d29e1539ae302334c4895d62930fec3b7a5449
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-09-05 18:37:52 +0300

    Create saveEnd function

--- Module-3/Personality-Emojis/js/index.js
+++ Module-3/Personality-Emojis/js/index.js
@@ -1,14 +1,24 @@
 const emojisEl = document.getElementById("emojis-el");
+const input = document.getElementById("emoji-input");
+const saveEnd = document.getElementById("save-btn-end");
 
 let emojis = ["💘", "😢", "💪"];
+
+saveEnd.addEventListener("click", function() {
+    const value = input.value;
+    if (value)
+    {
+        emojis.push(value);
+        render();
+    }
+});
 
 function render() {
     let str = "";
     for (let i = 0; i < emojis.length; i++)
     {
-        str += `<span>${emojis[i]}</span>`;
+        str += `<span>${emojis[i]} </span>`;
     }
     emojisEl.innerHTML = str;
 }
 
-render();
