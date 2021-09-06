6f35efe6a576397de43a01e8b57d8ae06aa1f007
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-09-06 20:12:01 +0300

    Style buttons and image

--- Module-4/Carousel/js/index.js
+++ Module-4/Carousel/js/index.js
@@ -1,5 +1,14 @@
 const body = document.getElementById("body");
+const image = document.getElementById("display-image");
 
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
 // body.style.background = "url(\"images/icon.png\")";
 
 console.log(body.style.backgroundImage);
+
+rightBtn.addEventListener("click", function() {
+    body.style.backgroundImage = "url(\"images/left-arrow.png\")";
+    image.src = "images/left-arrow.png";
+    console.log(image);
+});
