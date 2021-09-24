33c084d5b2f2b55591be8bb714648f2ec32d5120
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-09-24 22:25:03 +0300

    Add random image dog api

--- Module-7/BoredBot/js/index.js
+++ Module-7/BoredBot/js/index.js
@@ -1,4 +1,11 @@
 const btn = document.getElementById("btn");
 const idea = document.getElementById("idea");
+const body = document.querySelector("body");
 
-console.log(btn, idea);
+btn.addEventListener("click", () =>  {
+        
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(response => response.json())
+    .then(data => body.style.backgroundImage = `url(${data.message})`);
+
+});
