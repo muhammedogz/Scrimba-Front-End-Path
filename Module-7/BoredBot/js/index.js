b9f9c5ab4524f6d2daf959f1aaaa7af93c9cda7f
Muhammed Oguz <muhammedaslan1999@outlook.com>
2021-09-24 22:29:38 +0300

    Add random idea generator

--- Module-7/BoredBot/js/index.js
+++ Module-7/BoredBot/js/index.js
@@ -8,4 +8,11 @@ btn.addEventListener("click", () =>  {
     .then(response => response.json())
     .then(data => body.style.backgroundImage = `url(${data.message})`);
 
+
+    fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        document.getElementById("idea").textContent = data.activity;
+    });
 });
