937684d2b49b4dcc866102cd7713806fcd86c9cf
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-10-03 14:54:37 +0300

    Send new post to server

--- Module-7/BlogSpace/js/index.js
+++ Module-7/BlogSpace/js/index.js
@@ -5,39 +5,62 @@ const postTitle = document.getElementById("post-title");
 const postBody = document.getElementById("post-body");
 const btn = document.getElementById("post-submit");
 
+let posts = [];
+
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-    .then(res => res.json())
-    .then(data => {
-        data = data.slice(0, 5);
-        writeBlog(data);
-    });
+	.then((res) => res.json())
+	.then((data) => {
+		posts = data.slice(0, 5);
+		renderBlog(posts);
+	});
 
+function renderBlog(blog) {
+	let mainHTML = "";
 
-function writeBlog(blog) {
-    let mainHTML = "";
-
-    blog.forEach(item => {
-        mainHTML += `
+	blog.forEach((item) => {
+		mainHTML += `
         <article>
             <h2>${item.title}</h2>
             <p>${item.body}</p>
         </article>
         `;
-    });
-    main.innerHTML += mainHTML;
+	});
+	main.innerHTML = mainHTML;
 }
 
-postForm.addEventListener("submit",(e) => {
-    e.preventDefault();
-    console.log("clicked");
-    main.innerHTML += `<article>
-            <h2>${postTitle.value}</h2>
-            <p>${postBody.value}</p>
-        </article>
-        `;
+postForm.addEventListener("submit", (e) => {
+	e.preventDefault();
+
+    // create new object that keeps entered values
+	const newPost = {
+        title: postTitle.value,
+        body: postBody.value
+    };
+
+    // send new post to server 
+    // probably this is not working
+
+    // options object
+    const options = {
+        method: "POST",
+        body: JSON.stringify(newPost),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    };
+
+    fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+    .then(res => res.json())
+    .then(post => {
+        // add new post to html
+        posts.unshift(post);
+        renderBlog(posts);
+
+    });
+
+
+    // clear values
+    postTitle.value = "";
+    postBody.value = "";
+
 });
-
-
-
-
-
