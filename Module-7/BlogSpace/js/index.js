815127f204cda36f9ff31650d768c9f61c48812d
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-10-03 13:44:45 +0300

    Add form functionality

--- Module-7/BlogSpace/js/index.js
+++ Module-7/BlogSpace/js/index.js
@@ -1,14 +1,15 @@
 const main = document.querySelector("main");
 
-console.log(main);
+const postTitle = document.getElementById("post-title");
+const postBody = document.getElementById("post-body");
+const btn = document.getElementById("post-submit");
 
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
     .then(res => res.json())
     .then(data => {
         data = data.slice(0, 5);
         writeBlog(data);
-    })
-
+    });
 
 
 function writeBlog(blog) {
@@ -20,11 +21,19 @@ function writeBlog(blog) {
             <h2>${item.title}</h2>
             <p>${item.body}</p>
         </article>
-        `
+        `;
     });
     main.innerHTML += mainHTML;
-    
 }
+
+btn.addEventListener("click",() => {
+    console.log("clicked");
+    main.innerHTML += `<article>
+            <h2>${postTitle.value}</h2>
+            <p>${postBody.value}</p>
+        </article>
+        `;
+});
 
 
 
