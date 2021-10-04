d8bf6a4037a35ca2b9b75a142f64ff2d5009f3bd
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-10-04 12:35:24 +0300

    Add body flex class

--- Module-7/War-Game/js/index.js
+++ Module-7/War-Game/js/index.js
@@ -1,5 +1,7 @@
-const newGameBtn = document.getElementById("new-game");
+const bodyEl = document.querySelector("body");
 const mainEl = document.querySelector("main");
+
+const newGameBtn = document.getElementById("new-game");
 const computerScore = document.getElementById("computer-score");
 const playerScore = document.getElementById("player-score");
 const remainingCard = document.querySelector("#remaining");
@@ -13,9 +15,11 @@ newGameBtn.addEventListener("click", () => {
 function startRestartGame() {
     if (mainEl.classList.contains("display-none")) {
         mainEl.classList.remove("display-none");
+        bodyEl.classList.remove("flex");
         newGameBtn.innerText = "Reset Game";
     } else {
         mainEl.classList.add("display-none");
+        bodyEl.classList.add("flex");
         newGameBtn.innerText = "Start Game";
         remaining = 56;
         remainingCard.innerText = remaining;
@@ -23,3 +27,9 @@ function startRestartGame() {
         playerScore.innerText = "0";
     }
 }
+
+fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    });
