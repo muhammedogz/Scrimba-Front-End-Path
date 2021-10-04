3b48449db578bd4f745d93991064d07e33a73c14
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-10-04 16:18:25 +0300

    Add draw images and funcionality

--- Module-7/War-Game/js/index.js
+++ Module-7/War-Game/js/index.js
@@ -2,14 +2,44 @@ const bodyEl = document.querySelector("body");
 const mainEl = document.querySelector("main");
 
 const newGameBtn = document.getElementById("new-game");
+const drawBtn = document.getElementById("draw-card");
+
+const cardPlayer = document.getElementById("cardPlayer");
+const cardComputer = document.getElementById("cardComputer");
+
 const computerScore = document.getElementById("computer-score");
 const playerScore = document.getElementById("player-score");
 const remainingCard = document.querySelector("#remaining");
 
-let remaining = 56;
+let remaining = 52;
+let deckId = 0;
+
 
 newGameBtn.addEventListener("click", () => {
-    startRestartGame();
+    fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then(res => res.json())
+    .then(data => {
+        deckId = data.deck_id;
+    });
+    //startRestartGame();
+});
+
+drawBtn.addEventListener("click", () => {
+    let cards = [];
+    
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.cards[0].value, data.cards[1].value);
+        cards = data.cards;
+        cardPlayer.style.backgroundImage = `url(${cards[0].image})`;
+        cardComputer.style.backgroundImage = `url(${cards[1].image})`;
+
+        remaining = data.remaining;
+        remainingCard.innerText = remaining;
+    });
+
+    
 });
 
 function startRestartGame() {
@@ -28,8 +58,6 @@ function startRestartGame() {
     }
 }
 
-fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-    });
+
+
+
