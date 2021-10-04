4f2d931c6ab87789531784b0d899ca4886a40608
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-10-04 16:58:09 +0300

    Add checkWinner function

--- Module-7/War-Game/js/index.js
+++ Module-7/War-Game/js/index.js
@@ -9,7 +9,14 @@ const cardComputer = document.getElementById("cardComputer");
 
 const computerScore = document.getElementById("computer-score");
 const playerScore = document.getElementById("player-score");
+let playerScoreValue = 0;
+let computerScoreValue = 0;
+
 const remainingCard = document.querySelector("#remaining");
+
+const allCards = ["2","3","4","5","6","7","8","9","JACK","QUEEN","KING","ACE"];
+
+console.log(allCards.indexOf("JACK"));
 
 let remaining = 52;
 let deckId = 0;
@@ -21,25 +28,16 @@ newGameBtn.addEventListener("click", () => {
     .then(data => {
         deckId = data.deck_id;
     });
-    //startRestartGame();
+    startRestartGame();
 });
 
-drawBtn.addEventListener("click", () => {
-    let cards = [];
-    
+drawBtn.addEventListener("click", () => {   
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
     .then(res => res.json())
     .then(data => {
-        console.log(data.cards[0].value, data.cards[1].value);
-        cards = data.cards;
-        cardPlayer.style.backgroundImage = `url(${cards[0].image})`;
-        cardComputer.style.backgroundImage = `url(${cards[1].image})`;
-
-        remaining = data.remaining;
-        remainingCard.innerText = remaining;
+        renderPage(data.cards, data.remaining);
+        checkWinner(data.cards[0], data.cards[1]);
     });
-
-    
 });
 
 function startRestartGame() {
@@ -55,6 +53,23 @@ function startRestartGame() {
         remainingCard.innerText = remaining;
         computerScore.innerText = "0";
         playerScore.innerText = "0";
+    }
+}
+
+function renderPage(cards, remaining) {
+    cardPlayer.style.backgroundImage = `url(${cards[0].image})`;
+    cardComputer.style.backgroundImage = `url(${cards[1].image})`;
+    remainingCard.innerText = remaining;
+}
+
+function checkWinner(player, computer) {
+    const valuePlayer = allCards.indexOf(player.value);
+    const valueComputer = allCards.indexOf(computer.value);
+
+    if (valuePlayer > valueComputer) {
+        playerScore.innerText = ++playerScoreValue;
+    } else if (valueComputer > valuePlayer) {
+        computerScore.innerText = ++computerScoreValue;
     }
 }
 
