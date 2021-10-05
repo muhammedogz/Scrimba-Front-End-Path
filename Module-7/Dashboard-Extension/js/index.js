9d4a6412c58faaeb2d02f159b4efb0f1ac9e7c8f
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-10-05 12:48:54 +0300

    Add text-shadow fix links

--- Module-7/Dashboard-Extension/js/index.js
+++ Module-7/Dashboard-Extension/js/index.js
@@ -1,8 +1,12 @@
-const body = document.querySelector("body");
+const bodyEl = document.querySelector("body");
+const imageAuthor = document.getElementById("image-author");
+const imageInfo = document.getElementById("image-info");
 
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=tech")
     .then(res => res.json())
     .then(data => {
-        console.log(data.urls);
-        body.style.backgroundImage = `url(${data.urls.regular})`;
+        console.log(data);
+        imageAuthor.textContent = data.user.name;
+        imageInfo.setAttribute("href", data.links.html);
+        bodyEl.style.backgroundImage = `url(${data.urls.small})`;
     });
