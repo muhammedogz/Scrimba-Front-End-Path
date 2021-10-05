ff5a4b875fb92b48b7a5999befc1a3b0fbefd1a7
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-10-05 14:05:03 +0300

    Add currency info

--- Module-7/Dashboard-Extension/js/index.js
+++ Module-7/Dashboard-Extension/js/index.js
@@ -2,6 +2,10 @@ const bodyEl = document.querySelector("body");
 const imageAuthor = document.getElementById("image-author");
 const imageInfo = document.getElementById("image-info");
 
+const dollar = document.getElementById("dollar");
+const euro = document.getElementById("euro");
+const pound = document.getElementById("pound");
+
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=asdasdasd")
     .then(res => res.json())
     .then(data => {
@@ -17,3 +21,21 @@ fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         imageInfo.setAttribute("href", "https://unsplash.com/photos/dnPniNPUe4o");
         
     });
+
+
+function getCurrencyTRY(currency, symbol) {
+    fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${symbol}/try.json`)
+        .then(res => res.json())
+        .then(data => {
+            const str = "" + data.try;
+            currency.textContent = str.slice(0,5);
+        })
+        .catch(err => {
+            currency.style.fontSize = "1.1rem";
+            currency.textContent = "An error occurred";
+        });
+}
+
+getCurrencyTRY(dollar, "usd");
+getCurrencyTRY(euro, "eur");
+getCurrencyTRY(pound, "gbp");
