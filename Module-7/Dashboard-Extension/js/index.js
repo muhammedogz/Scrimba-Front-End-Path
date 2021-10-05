8b02d76fd004a821701b0820bf727dd6c1255503
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-10-05 13:18:03 +0300

    Handle rejected promise

--- Module-7/Dashboard-Extension/js/index.js
+++ Module-7/Dashboard-Extension/js/index.js
@@ -2,11 +2,18 @@ const bodyEl = document.querySelector("body");
 const imageAuthor = document.getElementById("image-author");
 const imageInfo = document.getElementById("image-info");
 
-fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=tech")
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=asdasdasd")
     .then(res => res.json())
     .then(data => {
         console.log(data);
-        imageAuthor.textContent = data.user.name;
+        imageAuthor.textContent = "By: " + data.user.name;
         imageInfo.setAttribute("href", data.links.html);
         bodyEl.style.backgroundImage = `url(${data.urls.small})`;
+    })
+    .catch(err => {
+        console.error(err);
+        bodyEl.style.backgroundImage = `url(images/default-bg.jpg)`;
+        imageAuthor.textContent = "By: " + "Dhaval Parmar";
+        imageInfo.setAttribute("href", "https://unsplash.com/photos/dnPniNPUe4o");
+        
     });
