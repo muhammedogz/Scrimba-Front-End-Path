80ebfcfa587d0f671a3e4fb90723d031b4ef5edc
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-10-06 23:53:30 +0300

    Add images

--- Module-8/Photo-Gallery/js/index.js
+++ Module-8/Photo-Gallery/js/index.js
@@ -1,1 +1,25 @@
 const bodyEl = document.querySelector("body");
+
+const photoCount = 20; // max 100
+
+async function getPhotos(count) {
+    const res = await fetch(`https://picsum.photos/v2/list?page=2&limit=${count}`);
+    const photos = await res.json();
+    const p = photos[0].download_url;
+    console.log(p.slice(0,p.length - 10));
+
+    let html = "<div class='photos'>";
+
+    for (const photo of photos) {
+        let url = photo.download_url;
+        url = url.slice(0, url.length - 10) + "/200/300";
+        
+        html += `<img src="${url}" alt="Author: ${photo.author}">`;
+    
+    }
+    html += "</div>";
+    bodyEl.innerHTML += html;
+    
+}
+
+getPhotos(photoCount);
