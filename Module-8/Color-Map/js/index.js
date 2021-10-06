c572b11d13d69e09744010065c6545d4468d09e0
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-10-06 16:38:44 +0300

    Make colorfull bg

--- Module-8/Color-Map/js/index.js
+++ Module-8/Color-Map/js/index.js
@@ -1,10 +1,24 @@
 const bodyEl = document.querySelector("body");
 
-async function getColors() {
-    const res = await fetch("https://apis.scrimba.com/hexcolors/?count=2");
-    console.log(res);
+const colorCount = 100;
+
+async function getColors(count) {
+    const res = await fetch(`https://apis.scrimba.com/hexcolors/?count=${count}`);
     const data = await res.json();
     return data;
 }
 
-getColors();
+getColors(colorCount)
+    .then(data => {
+        let html = "<div class='colors'>";
+        for (const color of data.colors) {
+            html += `
+    <div class=color style="background-color: ${color.value};">
+        
+    </div>`;
+        }
+        html += "\n</div>";
+
+        console.log(html);
+        bodyEl.innerHTML = html;
+    });
