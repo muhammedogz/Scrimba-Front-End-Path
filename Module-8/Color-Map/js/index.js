015880eed7cd313e34d1a018b6f4ec25cfd274b7
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-10-06 16:48:47 +0300

    Refactor

--- Module-8/Color-Map/js/index.js
+++ Module-8/Color-Map/js/index.js
@@ -5,20 +5,19 @@ const colorCount = 100;
 async function getColors(count) {
     const res = await fetch(`https://apis.scrimba.com/hexcolors/?count=${count}`);
     const data = await res.json();
-    return data;
+    return data.colors;
 }
 
+
 getColors(colorCount)
-    .then(data => {
+    .then(colors => {
         let html = "<div class='colors'>";
-        for (const color of data.colors) {
+        for (const color of colors) {
             html += `
     <div class=color style="background-color: ${color.value};">
         
     </div>`;
         }
         html += "\n</div>";
-
-        console.log(html);
         bodyEl.innerHTML = html;
     });
