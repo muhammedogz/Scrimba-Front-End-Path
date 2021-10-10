4fa0509045e997b319494c422cefd6b93d4a6634
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-10-10 23:22:23 +0300

    Code is not working

---
+++ Module-8/Jeopardy/js/index.js
@@ -0,0 +1,51 @@
+const bodyEl = document.querySelector("body");
+
+const categoryArr = () => {
+    const arr = [];
+    while (true) {
+        const random = Math.floor(Math.random() * 24) + 9;
+        if (!arr.includes(random)) 
+            arr.push(random);
+        if (arr.length == 4) break;
+    }
+
+    return arr;
+};
+
+
+async function getCategories(categoryId) {
+    const res = await fetch(`https://opentdb.com/api.php?amount=4&category=${categoryId}&type=boolean`);
+    const data = await res.json();
+    return data.results;
+}
+
+async function getQuestions() {
+    const questionsArr = [];
+    const randomArr = categoryArr();
+    console.log('randomArr :>> ', randomArr);
+    randomArr.forEach(rand => {
+        getCategories(rand).then(questions => {
+            console.log(questions);
+            questionsArr.push(questions);
+        });
+    }); 
+    console.log('questionsArr :>> ', questionsArr);
+    return questionsArr;
+}
+
+async function renderQuestions() {
+    getQuestions().then(categories => {
+        console.log('categories :>> ', categories);
+        let questionHTML = "";
+        console.log('categories.length :>> ', categories.length);
+        categories.forEach(questions => {
+                console.log('questions :>> ', questions);
+        });
+        for (let i = 0; i < 4; i++) console.log('ili :>> ', categories[i]);
+    });
+    
+}
+
+renderQuestions();
+
+
