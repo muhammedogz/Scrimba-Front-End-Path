df1dc68203eb38848609911ba1d27d79b40fdadd
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-10-10 17:53:44 +0300

    Add user handler

---
+++ Module-8/Online-Users/js/index.js
@@ -0,0 +1,25 @@
+const bodyEl = document.querySelector("body");
+
+
+async function getUsers() {
+    let response = await fetch("users.json");
+    let users = await response.json();
+    return users;
+}
+
+getUsers().then(renderUsers);
+
+function renderUsers(users) {
+    let userHtml = "<div class='online-users'>";
+
+    for (const user of users) {
+        console.log(user.name);
+        userHtml += `<div class="online-user">
+        <p class="user-name">${user.name}</p>
+        <div class="online-status"></div>\n</div>\n`;
+    }
+    userHtml += "</div>";
+    console.log(userHtml);
+
+    bodyEl.innerHTML += userHtml;
+}
