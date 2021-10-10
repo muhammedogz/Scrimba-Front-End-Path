438b909f963883d493cad5668bb3f01b790e692d
Muhammed Oguz <m.oguz2018@gtu.edu.tr>
2021-10-10 18:21:19 +0300

    Add general styling and functionality

---
+++ Module-8/User-Profile/js/index.js
@@ -0,0 +1,35 @@
+
+
+async function getUser() {
+    let response = await fetch("https://jsonplaceholder.typicode.com/users/3");
+    let user = await response.json();
+    return user;
+}
+
+function displayUser(user) {
+    document.body.innerHTML = `<div class="user-profile">
+        <div class="user-profile-header">
+            <div>${user.name}</div>
+            <div>@${user.username}</div>
+        </div>
+        
+        <div class="user-profile-company">
+            <div>Company: ${user.company.name}</div>
+            <div>${user.company.catchPhrase}</div>
+            <div>${user.company.bs}</div>
+        </div>
+        
+        <div class="user-profile-contact">
+            <div>📧${user.email}</div>
+            <div>📞${user.phone}</div>
+            <div>💻${user.website}</div>
+        </div>
+        
+        <div class="user-profile-address">
+            <div>${user.address.street}, ${user.address.suite}</div>
+            <div>${user.address.city} ${user.address.zipcode}</div>
+        </div>
+    </div>`;
+}
+
+getUser().then(displayUser);
